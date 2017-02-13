71cf63e6cef1b45848c0b35deaf479ce8053bc6f
Brian Okoyo <okoyobrian@users.noreply.github.com>
2017-02-13 10:49:09 +0300

    Add files via upload

---
+++ example/src/main/java/org/sufficientlysecure/htmltextview/example/MainActivity.java
@@ -0,0 +1,52 @@
+package org.sufficientlysecure.htmltextview.example;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.text.Html;
+import android.view.View;
+
+import org.sufficientlysecure.htmltextview.ClickableTableSpan;
+import org.sufficientlysecure.htmltextview.DrawTableLinkSpan;
+import org.sufficientlysecure.htmltextview.HtmlTextView;
+
+import static org.sufficientlysecure.htmltextview.example.WebViewActivity.EXTRA_TABLE_HTML;
+
+public class MainActivity extends Activity {
+
+    // The html table(s) are individually passed through to the ClickableTableSpan implementation
+    // presumably for a WebView activity.
+    class ClickableTableSpanImpl extends ClickableTableSpan {
+        @Override
+        public ClickableTableSpan newInstance() {
+            return new ClickableTableSpanImpl();
+        }
+
+        @Override
+        public void onClick(View widget) {
+            Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+            intent.putExtra(EXTRA_TABLE_HTML, getTableHtml());
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        HtmlTextView text = (HtmlTextView) findViewById(R.id.html_text);
+
+        text.setRemoveFromHtmlSpace(true);
+        text.setClickableTableSpan(new ClickableTableSpanImpl());
+        DrawTableLinkSpan drawTableLinkSpan = new DrawTableLinkSpan();
+        drawTableLinkSpan.setTableLinkText("[tap for table]");
+        text.setDrawTableLinkSpan(drawTableLinkSpan);
+        text.setHtmlFromString(getResources().getString(R.string.test_html),
+                new HtmlTextView.LocalImageGetter());
+    }
+
+}
